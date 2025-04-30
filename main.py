9d755024a13f0af63c3fdacdfbba5000ba0522fd
Freedom9100 <diskor07@gmail.com>
2025-04-30 14:47:33 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackContext
+import requests
+import os
+
+API_URL = "http://localhost:3000/api/posts/telegram-post"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def handle_message(update: Update, context: CallbackContext):
+    if update.channel_post:
+        content = update.channel_post.text or ""
+
+        data = {
+            "content": content,
+        }
+
+        try:
+            headers = {
+                "X-Secret-Key": "secret"
+            }
+
+            requests.post(API_URL, json=data, headers=headers)
+        except Exception as e:
+            print(f"Ошибка при отправке данных: {e}")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.ALL, handle_message))
+
+print("Bot started")
+
+app.run_polling()
