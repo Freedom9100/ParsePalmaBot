67ba80a8ffa44fe8e5ee9f4f98cd8760db9d0109
Freedom9100 <diskor07@gmail.com>
2025-04-30 14:58:30 +0300

    url update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackCo
 import requests
 import os
 
-API_URL = "http://localhost:3000/api/posts/telegram-post"
+API_URL = "http://palma-forum.io/api/posts/telegram-post"
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def handle_message(update: Update, context: CallbackContext):
