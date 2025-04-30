4faf49796c293208afcf2201e7facf228121c460
Freedom9100 <diskor07@gmail.com>
2025-04-30 20:37:11 +0300

    Testing v1

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackContext
 from telethon import TelegramClient, events
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Application, CommandHandler, ContextTypes
 import requests
 import os
 import json
 
 API_URL = "http://localhost:3000/api/posts/telegram-post"
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBAPP_URL = ""
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 
@@ -14,6 +17,12 @@ client = TelegramClient("session", api_id, api_hash)
 
 with open("channels.json", "r", encoding="utf-8") as f:
     telegram_channels = json.load(f)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Открыть админ-панель", web_app=WebAppInfo(url=WEBAPP_URL))]
+    ])
+    await update.message.reply_text("Нажми кнопку ниже для входа в админку:", reply_markup=keyboard)
 
 @client.on(events.NewMessage(chats=telegram_channels))
 async def handle_message(update: Update, context: CallbackContext):
